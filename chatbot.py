6a9d488282ce69fa627281ceaa026d3e2ac2d00d
MinJung, Kim <128272106+coco1718@users.noreply.github.com>
2023-07-02 16:08:06 +0900

    Add files via upload
    SentenceTransformer 를 활용한 감성챗봇

---
+++ chatbot.py
@@ -0,0 +1,54 @@
+# https://colab.research.google.com/drive/1g40AYlFhSQaVJYCMmLmQVJLdxHtpvXMn 
+# 위페이지에서 원본데이터셋 수정
+#import streamlit as st
+#st.title("Hello!")
+
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+
+#https://docs.streamlit.io/library/advanced-features/caching
+#@st.cache_resource(allow_output_mutation=True)
+@st.cache_resource
+def cached_model():
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+    return model
+
+@st.cache_resource
+def get_dataset():
+    df = pd.read_csv('wellness_dataset.csv')
+    df['embedding'] = df['embedding'].apply(json.loads)
+    return df
+
+model = cached_model()
+df = get_dataset()
+
+st.header('착한 챗봇')
+st.markdown("[❤️화성민의 별](https://www.youtube.com/c/빵형의개발도상국)")
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+with st.form('form', clear_on_submit=True):
+    user_input = st.text_input('당신: ', '')
+    submitted = st.form_submit_button('전송')
+
+if submitted and user_input:
+    embedding = model.encode(user_input)
+
+    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    answer = df.loc[df['distance'].idxmax()]
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(answer['챗봇'])
+
+for i in range(len(st.session_state['past'])):
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
